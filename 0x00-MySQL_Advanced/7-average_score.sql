183d7e4f7d8c8a87b5f438caf3827caf0049dc6f
Gideon <kipkerring.gedion@gmail.com>
2023-10-13 08:37:36 +0000

    a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,25 @@
+-- a SQL script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INTEGER
+)
+BEGIN
+	DECLARE total_score DECIMAL(10,2);
+	DECLARE total_corrections INTEGER;
+
+	SELECT SUM(score) INTO total_score
+	FROM corrections
+	WHERE user_id = user_id;
+
+	SELECT COUNT(*) INTO total_corrections
+	FROM corrections
+	WHERE user_id = user_id;
+
+	IF total_corrections > 0 THEN
+		UPDATE users
+		SET average_score = total_score / total_corrections
+		WHERE id = user_id;
+	END IF;
+END ; $$
+DELIMITER ;
