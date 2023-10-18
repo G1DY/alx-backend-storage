2c4829b2f27e0105cd85d1633e1864994092e421
Gideon <kipkerring.gedion@gmail.com>
2023-10-18 15:28:07 +0000

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
-"""declares a redis class and methodS"""
+"""Create a Cache class. In the __init__ method, store an instance
+   of the Redis client as a private variable named _redis (using 
+   redis.Redis()) and flush the instance using flushdb
+
+   In this exercise we will create a get method that take a key 
+   string argument and an optional Callable argument named fn. 
+   This callable will   be used to convert the data back to the desired format
+
+   Create and return function that increments the count for that key every 
+   time the method is called and returns the value returned by the original
+
+   In call_history, use the decorated function’s qualified name and append
+   ":inputs" and ":outputs" to create input and output list keys, respectively.
+
+   In this tasks, we will implement a replay function to display the history
+   of calls of a particular function.
+
+
+
+"""
 import redis
 from uuid import uuid4
 from functools import wraps
