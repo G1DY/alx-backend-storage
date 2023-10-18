53002a414f59b543f2bb2afdde4692f018acd49f
Gideon <kipkerring.gedion@gmail.com>
2023-10-18 18:19:34 +0000

    Familiarize yourself with redis commands RPUSH, LPUSH, LRANGE, etc.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,34 +22,6 @@ from functools import wraps
 from typing import Union, Callable, Optional
 
 
-def call_history(method: Callable) -> Callable:
-    """A decorator to store the history of inputs and outputs for a method."""
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        # Create keys for input and output lists
-        inputs_key = f"{method.__qualname__}:inputs"
-        outputs_key = f"{method.__qualname__}:outputs"
-
-        # Normalize input arguments to strings
-        input_str = str(args)
-
-        # Use Redis RPUSH to append input arguments to the inputs list
-        self._redis.rpush(inputs_key, input_str)
-
-        # Call the original method to retrieve the output
-        output = method(self, *args, **kwargs)
-
-        # Normalize the output to a string
-        output_str = str(output)
-
-        # Use Redis RPUSH to append the output to the outputs list
-        self._redis.rpush(outputs_key, output_str)
-
-        # Return the output
-        return output
-
-    return wrapper
-
 def replay(fn: Callable):
     r = redis.Redis()
     function_name = fn.__qualname__
@@ -60,7 +32,7 @@ def replay(fn: Callable):
         value = 0
 
     print(f"{function_name} was called {value} times:")
-
+    
     inputs = r.lrange(f"{function_name}:inputs", 0, -1)
     outputs = r.lrange(f"{function_name}:outputs", 0, -1)
 
@@ -68,33 +40,3 @@ def replay(fn: Callable):
         input_str = input_str.decode("utf-8")
         output_str = output_str.decode("utf-8")
         print(f"{function_name}({input_str}) -> {output_str}")
-
-class Cache:
-    def __init__(self):
-        self._redis = redis.Redis(host='localhost', port=6379, db=0)
-        self._redis.flushdb()
-
-    @call_history  # Decorate the store method with call_history
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        rkey = str(uuid4())
-        self._redis.set(rkey, data)
-        return rkey
-
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
-        value = self._redis.get(key)
-        if fn:
-            value = fn(value)
-        return value
-
-    def get_str(self, key: str) -> str:
-        value = self._redis.get(key)
-        return value.decode("utf-8")
-
-    def get_int(self, key: str) -> int:
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode("utf-8"))
-        except Exception:
-            value = 0
-        return value
-
