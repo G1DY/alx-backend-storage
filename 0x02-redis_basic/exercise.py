c0f53e8e45b31d77c4cb6d1ead28f16683dec2f2
Gideon <kipkerring.gedion@gmail.com>
2023-10-18 14:25:45 +0000

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -66,3 +66,11 @@ class Cache:
         except Exception:
             value = 0
         return value
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf")
+    }
