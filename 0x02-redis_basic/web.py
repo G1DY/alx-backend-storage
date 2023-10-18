9155f8906ac3abae4b5a2cf7cbd200dd758f7da4
Gideon <kipkerring.gedion@gmail.com>
2023-10-18 22:59:29 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,28 +5,25 @@ web cache and tracker
 import requests
 import redis
 from functools import wraps
+from typing import Callable
 
 
 store = redis.Redis()
 
 
-def count_url_access(method):
+def count_url_access(method: Callable) -> Callable:
     """ Decorator counting how many times
     a URL is accessed """
     @wraps(method)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        store.incr(f'count:{url}')
+        result = store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        store.set(f'count:{url}', 0)
+        store.setex(f'result:{url}', 10, result)
+        return result
     return wrapper
 
 
