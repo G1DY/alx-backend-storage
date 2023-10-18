a400f3034e7119eddc598916b3ccdae8302d5117
Gideon <kipkerring.gedion@gmail.com>
2023-10-18 22:06:59 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,43 +4,34 @@
 """
 import requests
 import redis
+from functools import wraps
 
 
-# Initialize the Redis client
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis()
 
 
+def access_count(method):
+    """decorator for our function"""
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
+
+
+@access_count
 def get_page(url: str) -> str:
-    # Define the key for counting access to this URL
-    count_key = f"count:{url}"
-    # Try to get the access count from Redis
-    access_count = redis_client.get(count_key)
-    
-    if access_count is None:
-        access_count = 0
-    else:
-        access_count = int(access_count)
-    
-    # If the count exceeds 10, reset the count to 0 and get the page
-    if access_count >= 10:
-        access_count = 0
-
-    # Increment the access count
-    redis_client.set(count_key, access_count + 1)
-
-    # Define the cache key for the page content
-    cache_key = f"cache:{url}"
-    cached_page = redis_client.get(cache_key)
-
-    if cached_page is not None:
-        # If the page is in the cache, return it
-        return cached_page.decode("utf-8")
-    else:
-        # If not in the cache, fetch the page
-        response = requests.get(url)
-        page_content = response.text
-
-        # Cache the page content with a 10-second expiration
-        redis_client.setex(cache_key, 10, page_content)
-
-        return page_content
+    """obtains html content"""
+    results = requests.get(url)
+    return results.text
