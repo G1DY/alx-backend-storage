51a34a056dc9c943f25b2c06e38e4905ae28e345
Gideon <kipkerring.gedion@gmail.com>
2023-10-18 15:19:10 +0000

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,24 +6,64 @@ from functools import wraps
 from typing import Union
 
 
-def call_history(fn):
-    """decorator to track method call history"""
-    @wraps(fn)
+def count_calls(method: Callable) -> Callable:
+    """returns a Callable"""
+    key = method.__qualname__
+
+    @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """returns the wrapper"""
-        result = fn(self, *args, **kwargs)
-        self._redis.lpush(f"{fn.__name__}_history", result)
-        return result
+        """wrapper for decorated function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
     return wrapper
 
-def call_count(fn):
-    """decorator to track method call history"""
-    @wraps(fn)
+
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs"""
+    @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """returns the wrapper"""
-        self._redis.incr(f"{fn.__name__}_count")
-        return fn(self, *args, **kwargs)
+        """wrapper for the decorated function"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+
     return wrapper
+
+
+def replay(fn: Callable):
+    """display the history of calls of a particular function"""
+    r = redis.Redis()
+    function_name = fn.__qualname__
+    value = r.get(function_name)
+    try:
+        value = int(value.decode("utf-8"))
+    except Exception:
+        value = 0
+
+    # print(f"{function_name} was called {value} times")
+    print("{} was called {} times:".format(function_name, value))
+    # inputs = r.lrange(f"{function_name}:inputs", 0, -1)
+    inputs = r.lrange("{}:inputs".format(function_name), 0, -1)
+
+    # outputs = r.lrange(f"{function_name}:outputs", 0, -1)
+    outputs = r.lrange("{}:outputs".format(function_name), 0, -1)
+
+    for input, output in zip(inputs, outputs):
+        try:
+            input = input.decode("utf-8")
+        except Exception:
+            input = ""
+
+        try:
+            output = output.decode("utf-8")
+        except Exception:
+            output = ""
+
+        # print(f"{function_name}(*{input}) -> {output}")
+        print("{}(*{}) -> {}".format(function_name, input, output))
 
 
 class Cache:
