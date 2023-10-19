80d31fae3658c1fbd3403a70987e2c0d52b18ef2
Gideon <kipkerring.gedion@gmail.com>
2023-10-19 00:39:05 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,40 +2,38 @@
 """
 web cache and tracker
 """
-import requests
+from requests import get
 import redis
 from functools import wraps
 from typing import Callable
 
 
-store = redis.Redis()
+redis_client = redis.Redis()
 
 
-def count_url_access(method: Callable) -> Callable:
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
+def response_cached_or_not(fn: Callable) -> Callable:
+    """a decorator to cache a http request in redis"""
+    @wraps(fn)
     def wrapper(url):
-        store.incr(f'count:{url}')
-        result = store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        store.set(f'count:{url}', 0)
-        store.setex(f'result:{url}', 10, result)
+        """returns the decorator"""
+        redis_client.incr(f"count:{url}")
+        cached_response = redis_client.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis_client.setex(f"cached:{url}", 10, result)
         return result
     return wrapper
 
 
-@count_url_access
+@response_cached_or_not
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """makes http request to a certain end point"""
+    return get(url).text
 
 
 if __name__ == "__main__":
-    url = 'http://slowwly.robertomurray.co.uk'  # URL for testing, you can replace it with other URLs
+    url = 'http://slowwly.robertomurray.co.uk'
     page_content = get_page(url)
     print("HTML Content:")
     print(page_content)
